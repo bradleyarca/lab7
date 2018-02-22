1bc645b2104307234ca8714fe578fd1f49c66628
Bradley Arca <barca@ucsd.edu>
2018-02-22 10:35:59 -0800

    fix

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var handlebars = require('express3-handlebars')
 
 var index = require('./routes/index');
 var project = require('./routes/project');
-var viewAlt = require('./routes/viewAlt');
+
 // Example route
 // var user = require('./routes/user');
 
