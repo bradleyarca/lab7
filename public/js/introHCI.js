6fd1b3f37822855c2066fc3eba517948129bfe23
Bradley Arca <barca@ucsd.edu>
2018-02-22 02:11:21 -0800

    changes

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -5,9 +5,17 @@ $(document).ready(function() {
 	initializePage();
 });
 
+$('.likeBtn').click(function(e)
+{
+	e.preventDefault();
+	
+	ga("send", "event", 'like', 'click');
+})
+
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
 	// your code here
 }
+
